19282a0edff82451c20c3c7a292bf557e9975d7b
RandxHuang <wind04181986@gmail.com>
2019-12-27 22:15:49 +0800

    Add ctk datetime picker

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+//https://chronotruck.github.io/vue-ctk-date-time-picker/
+import VueCtkDateTimePicker from "vue-ctk-date-time-picker";
+import "vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css";
+Vue.component("VueCtkDateTimePicker", VueCtkDateTimePicker);
+
 Vue.config.productionTip = false;
 
 new Vue({
